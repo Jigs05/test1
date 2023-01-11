b61e0c5cb8147318fafd53c7c82b538593d421b1
Jingo Yap <yap.jingo@gmail.com>
2023-01-11 16:55:14 +0800

    first commit

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+  
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+  
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    <div>
+                    <h1> You are Logged In </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
